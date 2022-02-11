a90e2724211f895f1a9f8a5d6f8f1c8185c40688
Arkapratim Ghosh <arkapratimghosh2002@gmail.com>
2022-02-11 19:13:45 +0530

    first commit

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CakeContainer(){
+    return (
+        <div>
+            <h2>Cake Container</h2>
+            <button>Buy Cake</button>
+        </div>
+    )
+}
+export default CakeContainer
